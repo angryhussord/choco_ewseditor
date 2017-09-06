74a4addb21352b29d7b4de0852e35b25620fd4d7
Patrick Hufford <pahuff@microsoft.com>
2017-09-06 06:14:23 -0700

    1.18 initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'ewseditor'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=ewseditor&DownloadId=1646373&FileTime=131340889353370000&Build=21053'
+$installDir = "c:\tools\EwsEditor\";
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  unzipLocation = $installDir
+  checksum      = 'F2C2E9A5199E940D724CFD94B3528D4D3E53CA875F98697C660AF8CA20E88A02'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
